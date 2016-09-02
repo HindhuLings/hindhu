c24147aa2246099175914f77524aa07b66475f07
HindhuLings <hindhulings@gmail.com>
2016-09-02 11:01:51 +0530

    Update Reverse.java

--- Reverse.java
+++ Reverse.java
@@ -4,10 +4,11 @@ Class Reverse
 {
  public static void main(string ag[])
  {
-  Scanner s=new Scanner(Sysetm.in);
+  Scanner s=new Scanner(System.in);
   String s1=s.next();
   StringBuffer s2=new StringBuffer(s1);
   s2.reverse();
-  System.out.println(s2);
+  String s3=s2.toString();
+  System.out.println(s3);
   }
   }
